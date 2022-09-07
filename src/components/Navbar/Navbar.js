acdd16ffcf302dfef5b7260b0db1b1eab217aa9f
leonardopottmayer <pottmayerleonardo@gmail.com>
2022-09-07 00:04:29 -0300

    Trying to add a carousel

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logoPhotography from "../../assets/logos/logo_photography.png";
+
+import styles from "./Navbar.module.css";
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+function NavBar() {
+  return (
+    <Navbar className={styles.navbar_main} expand="lg">
+      <Navbar.Brand href="/">
+        <img
+          alt="Logo"
+          src={logoPhotography}
+          width="30"
+          height="30"
+          className={styles.nav_brand_logo}
+        />
+        Pottmayer Photography
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Link className={styles.nav_link} to="/">
+            Home
+          </Link>
+          <Link className={styles.nav_link} to="/photos">
+            Photos
+          </Link>
+          <a
+            className={styles.nav_link}
+            target="_blank"
+            href="https://www.pottmayer.dev"
+          >
+            Dev
+          </a>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavBar;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,6 +7,12 @@ import api from "../../services/api";
 import PhotoCard from "../../components/PhotoCard/PhotoCard";
 import { Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import Navbar from "../../components/Navbar/Navbar";
+import Carousel from "react-bootstrap/Carousel";
+
+import sunrisePottmayerPhoto from "../../assets/bannerImages/sunrise_pottmayer.jpg";
+import seaPottmayerPhoto from "../../assets/bannerImages/sea_pottmayer.jpg";
+import enchantPottmayerPhoto from "../../assets/bannerImages/enchant_pottmayer.jpg";
 
 const HomePage = () => {
   const [photos, setPhotos] = useState([]);
@@ -23,13 +29,53 @@ const HomePage = () => {
 
   return (
     <div className={styles.main}>
+      <Navbar />
+      <Carousel className={styles.carousel}>
+        <Carousel.Item className={styles.carousel_item}>
+          <img
+            className={`${styles.carousel_item_img}`}
+            src={sunrisePottmayerPhoto}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className={styles.carousel_item}>
+          <img
+            className={`${styles.carousel_item_img}`}
+            src={seaPottmayerPhoto}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className={styles.carousel_item}>
+          <img
+            className={`${styles.carousel_item_img}`}
+            src={enchantPottmayerPhoto}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
       <Container fluid>
-      <Row className={styles.row}>
-        {photos.map((ph) => (
-          // <li key={ph._id}>{ph.title} </li>
-          <PhotoCard photo={ph} key={ph._id} />
-        ))}
-      </Row>
+        <Row className={styles.row}>
+          {photos.map((ph) => (
+            // <li key={ph._id}>{ph.title} </li>
+            <PhotoCard photo={ph} key={ph._id} />
+          ))}
+        </Row>
       </Container>
     </div>
   );
