fcdbd862c030b80f74d0fd669d024b51be6f59c5
leonardopottmayer <pottmayerleonardo@gmail.com>
2022-10-07 18:18:23 -0300

    Changed api route

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:7575",
-  baseURL: "https://pottmayer-photography-api.herokuapp.com",
+  baseURL: "https://api.photography.pottmayer.dev",
 });
 
 api.interceptors.request.use(
