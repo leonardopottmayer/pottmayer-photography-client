46967234850f70940a2286711396f59f7b12b863
leonardopottmayer <pottmayerleonardo@gmail.com>
2022-10-08 19:07:06 -0300

    Added loading animation and changed navbar icon

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from "./Loading.module.css";
+
+const Loading = () => {
+  return (
+    <div className={styles.lds_dual_ring}></div>
+  )
+}
+
+export default Loading
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -8,14 +8,18 @@ import Row from "react-bootstrap/Row";
 import PostCard from "../../components/Normal/PostCard/PostCard";
 import Navbar from "../../components/Normal/Navbar/Navbar";
 import HomeBanner from "../../components/Normal/HomeBanner/HomeBanner";
+import Loading from "../../components/Loading/Loading";
 
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
+  const [showLoading, setShowLoading] = useState(false);
 
   const fetchData = async () => {
+    setShowLoading(true);
     await api.get("/posts/all").then(({ data }) => {
       setPosts(data.result);
     });
+    setShowLoading(false);
   };
 
   useEffect(() => {
@@ -26,11 +30,15 @@ const HomePage = () => {
     <div className={styles.main}>
       <Navbar />
       <HomeBanner />
-      <Row className={styles.row}>
-        {posts.map((post) => (
-          <PostCard post={post} key={post._id} />
-        ))}
-      </Row>
+      {showLoading ? (
+        <Loading />
+      ) : (
+        <Row className={styles.row}>
+          {posts.map((post) => (
+            <PostCard post={post} key={post._id} />
+          ))}
+        </Row>
+      )}
     </div>
   );
 };
